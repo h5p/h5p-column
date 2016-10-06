27ea5966bf6750aba56797f18fc0c12144b34d62
Frode Petterson <frode.andre.petterson@gmail.com>
2016-10-06 11:36:10 +0200

    Fixes content rendering and resizing
    HFP-45

--- scripts/h5p-column.js
+++ scripts/h5p-column.js
@@ -1,59 +1,133 @@
-var H5P = H5P || {};
+H5P.Column = (function (EventDispatcher) {
 
-/**
- * Will render a column with potentially multiple content instances in it
- *
- * @param {Array} content
- * @param {int} contentId
- * @returns {H5P.Columns} Instance
- */
-H5P.Column = (function ($) {
-        
-  function C (content, contentId) {
+  /**
+   * Glossary Test Constructor
+   *
+   * @class
+   * @param {Object} params Describes task behavior
+   * @param {number} id Content identifier
+   * @param {Object} data User specific data to adapt behavior
+   */
+  function Column(params, id, data) {
     var self = this;
-    
-    var defaults = {
-      content: [],
-    };
-    this.params = $.extend(true, {}, defaults, content);
 
-    this.content = new Array();
-    this.$myDom;
-    
-    // Instantiate content instances
-    for (var i = 0; i < this.params.content.length; i++) {
-      var contentData = this.params.content[i];
-      var child = H5P.newRunnable(contentData, contentId);
-      child.on('resize', function() {
-        self.trigger('resize');
-      });
-      this.content.push(child);
-    }
+    // Initialize event inheritance
+    EventDispatcher.call(self);
+
+    // Column wrapper element
+    var wrapper;
+
+    // H5P content in the column
+    var instances = [];
+
+    /**
+     * @private
+     *
+     * @param {Object} content Parameters
+     */
+    var addRunnable = function (content) {
+      // Create container for content
+      var container = document.createElement('div');
+      container.classList.add('h5p-column-content');
+
+      // Content overrides
+      if (content.library.split(' ')[0] === 'H5P.Video') {
+        // Prevent video from growing endlessly since height is unlimited.
+        content.params.visuals.fit = false;
+      }
+
+      // Create content instance
+      var instance = H5P.newRunnable(content, id, H5P.jQuery(container), true); // TODO: Add content state ?
+
+      // Bubble events
+      bubbleUp(instance, 'resize', self);
+
+      // Keep track of all instances
+      instances.push(instance);
+
+      // Add to DOM wrapper
+      wrapper.appendChild(container);
+    };
+
+    /**
+     * Creates a wrapper and the column content the first time the column
+     * is attached to the DOM.
+     *
+     * @private
+     */
+    var createHTML = function () {
+      // Create wrapper
+      wrapper = document.createElement('div');
+
+      // Add content
+      for (var i = 0; i < params.content.length; i++) {
+        addRunnable(params.content[i]);
+      }
+    };
+
+    /**
+     * Attach the column to the given container
+     *
+     * @param {H5P.jQuery} $container
+     */
+    self.attach = function ($container) {
+      if (wrapper === undefined) {
+        // Create wrapper and content
+        createHTML();
+      }
+
+      // Add to DOM
+      $container.addClass('h5p-column').html('').append(wrapper);
+    };
+
+    // Resize children to fit inside parent
+    bubbleDown(self, 'resize', instances);
   }
 
-  // Function for attaching the pages to a dom element.
-  C.prototype.attach = function (target) {
-    var $target;
-    if (typeof(target) === "string") {
-      $target = $('#' + target);
-    }
-    else {
-      $target = $(target);
-    }
+  // Extends the event dispatcher
+  Column.prototype = Object.create(EventDispatcher.prototype);
+  Column.prototype.constructor = Column;
 
-    this.$myDom = $('<' + this.params.type + '>').appendTo($target);
+  /**
+   * Makes it easy to bubble events from parent to children
+   *
+   * @private
+   * @param {Object} origin Origin of the Event
+   * @param {string} eventName Name of the Event
+   * @param {Array} targets Targets to trigger event on
+   */
+  function bubbleDown(origin, eventName, targets) {
+    origin.on(eventName, function (event) {
+      if (origin.bubblingUpwards) {
+        return; // Prevent send event back down.
+      }
 
-    // Attach content
-    for (var i = 0; i < this.content.length; i++) {
-      var contentInstance = this.content[i];
-      var $contentHolder = $('<div class="h5p-column-content"></div>');
-
-      contentInstance.attach($contentHolder);
-      this.$myDom.append($contentHolder);
-    }
-
-    return this;
+      for (var i = 0; i < targets.length; i++) {
+        targets[i].trigger(eventName, event);
+      }
+    });
   };
 
-  return C;
-})(H5P.jQuery);
+  /**
+   * Makes it easy to bubble events from child to parent
+   *
+   * @private
+   * @param {Object} origin Origin of the Event
+   * @param {string} eventName Name of the Event
+   * @param {Object} target Target to trigger event on
+   */
+  function bubbleUp(origin, eventName, target) {
+    origin.on(eventName, function (event) {
+      // Prevent target from sending event back down
+      target.bubblingUpwards = true;
+
+      // Trigger event
+      target.trigger(eventName, event);
+
+      // Reset
+      target.bubblingUpwards = false;
+    });
+  }
+
+  return Column;
+})(H5P.EventDispatcher);
